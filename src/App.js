22f3eae41a6eda217c992b9ea5cc670d351d6c2a
KcBaines <cj.kcbaines2gmail.com>
2025-02-03 09:14:35 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 import Portfolio from "./components/Portfolio";
 import Skills from "./components/Skills";
 import logo from "./img/logo2.png";
-// import video from "../src/video/background-vid3.mp4";
+import video from "../src/video/bg-vid.mp4";
 import "./App.css";
 
 function App() {
@@ -41,9 +41,9 @@ function App() {
     <div className="App">
       <div class="background"></div>
       <div class="cursor"></div>
-      {/* <video autoPlay loop muted playsInline className="background-video">
+      <video autoPlay loop muted playsInline className="background-video">
         <source src={video} type="video/mp4" />
-      </video> */}
+      </video>
       <img src={logo} className="App-logo" alt="logo" />
       <Router>
         <Navbar />
